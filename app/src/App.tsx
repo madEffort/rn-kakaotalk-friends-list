8f2375f7e294fd4ebfaa2653910b5987ac43dc1f
Tobias Kim <madeffort0903@gmail.com>
2024-09-28 14:34:57 +0900

    project settings

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Header from './components/Header/Header';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Header />
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ app/src/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {IconButtonPropsTypes} from '../types/header.types';
+
+const IconButton = (props: IconButtonPropsTypes) => {
+  return (
+    <View style={{paddingHorizontal: 6}}>
+      <Ionicons name={props.name} size={24} color={'black'} />
+    </View>
+  );
+};
+
+export default () => {
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.title}>친구</Text>
+      <View style={{flexDirection: 'row'}}>
+        <IconButton name="search-outline" />
+        <IconButton name="person-add-outline" />
+        <IconButton name="musical-notes-outline" />
+        <IconButton name="settings-outline" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    backgroundColor: 'pink',
+  },
+  title: {
+    fontSize: 23,
+    fontWeight: '500',
+    color: 'black',
+  },
+});

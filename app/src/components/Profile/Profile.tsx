73c974fa428e1708c7a96808fc332e09cbad4058
Tobias Kim <madeffort0903@gmail.com>
2024-09-29 13:33:30 +0900

    apply styled components

--- app/src/components/Profile/Profile.tsx
+++ app/src/components/Profile/Profile.tsx
@@ -2,29 +2,51 @@ import React from 'react';
 import { Image, Text, View } from 'react-native';
 import { ProfilePropsTypes } from './types/profile.types';
 import Margin from '../common/Margin';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex-direction: row;
+`;
+
+const ProfileImage = styled.Image<{ size: number }>`
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  border-radius: ${props => props.size * 0.4}px;
+`;
+
+const TextContainer = styled.View`
+  justify-content: center;
+  margin-left: 10px;
+`;
+
+const NameText = styled.Text`
+  font-weight: 400;
+  font-size: 16px;
+  color: black;
+`;
+
+const IntroductionText = styled.Text<{ isMe: boolean }>`
+  font-size: 12px;
+  color: ${props => (props.isMe ? 'black' : 'grey')};
+`;
 
 export default (props: ProfilePropsTypes) => {
   const size = props.isMe ? 50 : 40;
 
   return (
-    <View style={{ flexDirection: 'row' }}>
-      <Image
-        source={{ uri: props.uri }}
-        style={{ width: size, height: size, borderRadius: size * 0.4 }}
-      />
-      <View style={{ justifyContent: 'center', marginLeft: 10 }}>
-        <Text style={{ fontWeight: '400', fontSize: 16, color: 'black' }}>
-          {props.name}
-        </Text>
+    <Container>
+      <ProfileImage source={{ uri: props.uri }} size={size} />
+      <TextContainer>
+        <NameText>{props.name}</NameText>
         {!!props.introduction && (
           <View>
             <Margin height={6} />
-            <Text style={{ fontSize: 12, color: 'grey' }}>
+            <IntroductionText isMe={props.isMe}>
               {props.introduction}
-            </Text>
+            </IntroductionText>
           </View>
         )}
-      </View>
-    </View>
+      </TextContainer>
+    </Container>
   );
 };
